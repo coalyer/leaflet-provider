910166ab8c12ce0efd30eadf42c61cdf6465b926
coalyer <liyang_qifengle@126.com>
2022-05-31 23:42:59 +0800

    feat:配置路由

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>这里是index页面</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>leaflet提供商登录</div>
+    <a-form ref="form" :model="form">
+      <a-form-item>
+        <a-input v-model="form.name"></a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input v-model="form.password" type="password"></a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" @click="onSubmit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () { }
+  }
+}
+</script>
+
+<style>
+</style>
