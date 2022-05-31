a0b90fff407ceb01af41d42f50328f360149da85
coalyer <liyang_qifengle@126.com>
2022-05-31 23:49:43 +0800

    feat:使用sass预处理器

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@ export default {
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <div>leaflet提供商登录</div>
-    <a-form ref="form" :model="form">
-      <a-form-item>
-        <a-input v-model="form.name"></a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input v-model="form.password" type="password"></a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-button type="primary" @click="onSubmit">登录</a-button>
-      </a-form-item>
-    </a-form>
+  <div class="login">
+    <div class="l-form">
+      <div class="l-tip">leaflet提供商登录</div>
+      <a-form ref="form" :model="form">
+        <a-form-item>
+          <a-input v-model="form.name"></a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input v-model="form.password" type="password"></a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" @click="onSubmit">登录</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
 </template>
 
@@ -32,5 +34,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  background: #000;
+  .l-form {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%) translateX(-50%);
+    width: 300px;
+    margin: auto;
+    border-radius: 8px;
+    background: #fff;
+    padding: 20px;
+    .l-tip {
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+    }
+    .ant-form {
+      width: 100%;
+      margin: auto;
+      margin-top: 20px;
+      .ant-form-item {
+        button {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
 </style>
